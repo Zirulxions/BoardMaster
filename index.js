eaf559d23a758770169fc6510da351fbddfb964c
Javier Delgado <javier_180796@hotmail.com>
2018-11-10 02:04:05 -0400

    Second commit

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var express = require('express')
-var app = express()
-var server = require('http').Server(app)
-var io = require('socket.io')(server)
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var user = ["usuario1","usuario2"]; //array de retorno, llenado por emergencia, debe llenarse automaticamente
 
 app.use(express.static('public'))
 
 io.on('connection', socket => {
-	console.log('Alguien se conecto')
+	console.log('Alguien se conecto');
 	socket.on('click', data => {
-		socket.broadcast.emit('begin', data)
-	})
+		socket.broadcast.emit('begin', data);
+	});
 	socket.on('drawing', data => {
-		socket.broadcast.emit('move', data)
-	})
-})
+		socket.broadcast.emit('move', data);
+	});
+	socket.on('userList', data =>{          // Establecer el socket
+		console.log(user);                  // para retorno de array
+		socket.broadcast.emit('user', user);// o eso creo
+	});
+});
 
-server.listen(8080, () => console.log('Corriendo...'))
+server.listen(4000, () => console.log('Corriendo...'))
--- public/app.js
+++ public/app.js
@@ -1,91 +1,132 @@
-'use strict'
+var user = []; //declarar el array
+
+function getNickname(){
+	var nickname = document.getElementById("nickname");
+	document.getElementById("user").hidden = true;
+	document.getElementById("board").hidden = false;
+	user.push(nickname); //inserto en el array
+	init();
+}
 
 class Pencil {
 
 	constructor (pizarra) {
-		this.started = false
-		this.color = '#000'
-		this.pizarra = pizarra
-		this.pizarra.strokeStyle = this.color
+		this.started = false;
+		this.color = '#000'; //establecer default color
+		this.pizarra = pizarra;
+		this.pizarra.strokeStyle = this.color;
 	}
 
 	changeColor () {
-		this.color = this.color == '#000' ? '#f00' : '#000'
-		this.pizarra.strokeStyle = this.color
+		var numero = (Math.floor(Math.random() * 10 ) + 1);
+		console.log(numero);
+		if(numero == 1){	
+			this.color = '#f00' //Red
+		} else if(numero == 2) {
+			this.color = '#770167'; //Purple
+		} else if(numero == 3){
+			this.color = '#f80'; //Orange
+		} else if(numero == 4){
+			this.color = '#0019fc'; //Blue
+		} else if(numero == 5){
+			this.color = '#fcf700'; //Yellow
+		} else if(numero == 6){
+			this.color = '#ff009d'; //Pink
+		} else if(numero == 7){
+			this.color = '#15fc00'; //Green
+		} else if(numero == 8){
+			this.color = '#ffffff'; //White
+		} else if(numero == 9){
+			this.color = '#814fff'; //Teal
+		} else if(numero == 10){
+			this.color = '#4e1414'; //Wine
+		} else {
+			this.color = '#000'; //Default Black
+		}
+		this.pizarra.strokeStyle = this.color;
 	}
 
 	mousedown (ev) {
-		this.begin(ev.layerX, ev.layerY)
-		this.started = true
+		this.begin(ev.layerX, ev.layerY);
+		this.started = true;
 		socket.emit('click', {
 			x: ev.layerX, 
 			y: ev.layerY
-		})
+		});
 	}
 
 	mousemove (ev) {
 		if (this.started) {
-			this.move(ev.layerX, ev.layerY)
+			this.move(ev.layerX, ev.layerY);
 			socket.emit('drawing', {
 				x: ev.layerX, 
 				y: ev.layerY,
 				color: this.color
-			})
+			});
 		}
 	}
 
 	mouseup (ev) {
 		if (this.started) {
-			this.mousemove(ev)
-			this.started = false
+			this.mousemove(ev);
+			this.started = false;
 		}
 	}
 
 	moveMsg (data) {
-		this.pizarra.strokeStyle = data.color
-		this.move(data.x, data.y)
-		this.pizarra.strokeStyle = this.color
+		this.pizarra.strokeStyle = data.color;
+		this.move(data.x, data.y);
+		this.pizarra.strokeStyle = this.color;
 	}
 
 	begin (x, y) {
-		this.pizarra.beginPath()
-		this.pizarra.moveTo(x, y)
+		this.pizarra.beginPath();
+		this.pizarra.moveTo(x, y);
 	}
 
 	move (x, y) {
-		this.pizarra.lineTo(x, y)
-		this.pizarra.stroke()
+		this.pizarra.lineTo(x, y);
+		this.pizarra.stroke();
+	}
+
+	users (user) {
+		this.user;
+		socket.emit('userList', user);
 	}
 }
 
-const socket = io.connect('http://localhost:8080', {'forceNew': true})
+const socket = io();
 
-const canvas = document.getElementById("pizarra")
-const pizarra = canvas.getContext("2d")
-const btn = document.getElementById('boton')
-init()
+const canvas = document.getElementById("pizarra");
+const pizarra = canvas.getContext("2d");
+const btn = document.getElementById('boton');
 
 function init () {
-	const pencil = new Pencil(pizarra)
+	const pencil = new Pencil(pizarra);
 
-	canvas.addEventListener('mousedown', ev_handler, false)
-	canvas.addEventListener('mousemove', ev_handler, false)
-	canvas.addEventListener('mouseup',	 ev_handler, false)
-	btn.addEventListener('click', ev_change, false)
+	canvas.addEventListener('mousedown', ev_handler, false);
+	canvas.addEventListener('mousemove', ev_handler, false);
+	canvas.addEventListener('mouseup',	 ev_handler, false);
+	btn.addEventListener('click', ev_change, false);
+	//es posible necesitar un ev_handler
 
-	socket.on('begin', io_begin)
-	socket.on('move', io_move)
+	socket.on('begin', io_begin);
+	socket.on('move', io_move);
+	socket.on('users', io_users);
 
 	function ev_handler (ev) {
-		pencil[ev.type](ev)
+		pencil[ev.type](ev);
 	}
 	function ev_change (ev) {
 		pencil.changeColor()
 	}
 	function io_begin (data) {
-		pencil.begin(data.x, data.y)
+		pencil.begin(data.x, data.y);
 	}
 	function io_move (data) {
-		pencil.moveMsg(data)
+		pencil.moveMsg(data);
 	}
+	function io_users(user){ // Supuestamente aqui envio el array
+		pencil.users(user);  // o esa es mi intencion
+	}                        // no se que puede faltar
 }
